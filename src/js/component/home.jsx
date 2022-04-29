2560add6796c758754620e7b3211774475e7f6fd
Chris-Durr <chrisdurr88@gmail.com>
2022-04-29 19:28:05 +0000

    me muero

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,6 +9,47 @@ export default function Home() {
 	const [tarea, setTarea] = useState("");
 	const [lista, setLista] = useState([]);
 
+	//WOLOLO 1
+
+	useEffect(() => {
+		subirToDo();
+	}, [lista]);
+
+	useEffect(() => {
+		descargarToDo();
+	}, []);
+
+	function subirToDo() {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(lista);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			redirect: "follow",
+			body: raw,
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/ChrisDurr",
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log(result);
+			})
+			.catch((error) => console.log("error", error));
+	}
+
+	function descargarToDo() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ChrisDurr")
+			.then((response) => response.json())
+			.then((result) => setLista(result))
+			.catch((error) => console.log("error", error));
+	}
+	//WOLOLO 2
 	function borrar(i) {
 		let re = lista.filter((valor, index) => {
 			return index != i;
@@ -26,7 +67,7 @@ export default function Home() {
 			/>
 			<button
 				onClick={() => {
-					setLista([...lista, tarea]);
+					setLista([...lista, { label: tarea, done: false }]);
 				}}>
 				Agregar ToDO
 			</button>
